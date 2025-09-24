15232ae59479203c1014828f8832f392ef717cb7
notrajveer <raia79898@gmail.com>
2025-09-24 21:50:56 +0530

    Removed unwanted files and folders

--- Chapter 1/01_variables.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-    int a;
-    a = 6;
-    printf("The output of this command is %d", a);
-    return 0;
-}
--- Problem Set 1/Celsius_Fahrenheit.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main() {
-    float celcius, fahrenheit;
-    printf("\n Enter the temperature in Celcius \n");
-    scanf ("%f", &celcius);
-    fahrenheit = (celcius * 9/5) + 32;
-    printf("The value of temperature in Fahrenheit is %f", fahrenheit);
-    return 0;
-}
